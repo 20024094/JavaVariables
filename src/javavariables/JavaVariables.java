c00db9aa85c8b58fde220e8b17fe0c4a148b6b51
20024094 <20024094@10.5.144.235>
2023-09-12 12:36:43 +0100

    Commit

--- src/javavariables/JavaVariables.java
+++ src/javavariables/JavaVariables.java
@@ -12,23 +12,8 @@ package javavariables;
 public class JavaVariables {
 
     public static void main(String[] args) {
-        String manufacturer = "Mercedes";
-        String model = "Benz";
-        String operator = "Taxi Inverness";
-        int yearOfManufacture = 2010;
-        int maxCapacity = 4;
-        int engines = 1;
-        float priceCar = 30000.4f;
-        char status = 'A';
-        
-        System.out.println("Car: " + manufacturer + " " + model);
-        System.out.println("---------------------------------");
-        System.out.println("Operator: " + operator);
-        System.out.println("Production Year: " + yearOfManufacture);
-        System.out.println("Max Capacity: " + maxCapacity);
-        System.out.println("Engines: " + engines);
-        System.out.println("Car Price in Euro: " + priceCar);
-        System.out.println("Status: " + status);
-    }
-    
+      // this is local variable
+      String name = "Daniel Barbu";
+      System.out.println(name);
+    } 
 }
